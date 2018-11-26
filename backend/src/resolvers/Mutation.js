df8d6d137f7330c71cd7324dcc256eeca075cac8
Ondřej Hliba <hliba.ondra@live.com>
2018-11-26 11:49:14 +0100

    Add CreateEvent Mutaion

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+const Mutations = {
+  async createEvent(parent, args, ctx, info) {
+    // TODO: Check if user is logged in
+    const event = await ctx.db.mutation.createEvent(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return event;
+  }
+};
 
 module.exports = Mutations;
