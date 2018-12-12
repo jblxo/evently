9f15f5bcc85d5defafbe5e24345fe85962b9eb0e
Ondřej Hliba <hliba.ondra@live.com>
2018-12-12 14:00:53 +0100

    Add Frontend User Authentication

--- frontend/pages/updateUser.js
+++ frontend/pages/updateUser.js
@@ -1,5 +1,10 @@
 import UpdateUser from '../components/UpdateUser';
+import PleaseSignIn from '../components/PleaseSignIn';
 
-const UpdateUserPage = props => <UpdateUser id={props.query.id} />;
+const UpdateUserPage = props => (
+  <PleaseSignIn>
+    <UpdateUser id={props.query.id} />
+  </PleaseSignIn>
+);
 
 export default UpdateUserPage;
