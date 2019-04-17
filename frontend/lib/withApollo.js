8e2c9900677c026135fbae8f3b4b839ff9546c58
Ondřej Hliba <hliba.ondra@live.com>
2019-04-17 14:02:41 +0200

    Initial packages for migration

--- frontend/lib/withApollo.js
+++ frontend/lib/withApollo.js
@@ -1,18 +1,25 @@
-import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 import { GRAPHQL_URL } from '../config';
 
-export default withApollo(
-  ({ headers }) =>
-    new ApolloClient({
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
       uri: GRAPHQL_URL,
-      request: operation => {
-        operation.setContext({
-          fetchOptions: {
-            credentials: 'include'
-          },
-          headers
-        });
-      }
+      credentials: 'same-origin'
     })
-);
+  ]),
+  cache: new InMemoryCache()
+});
