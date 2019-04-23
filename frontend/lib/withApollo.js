0633ed027b0d7e65f9429ac0dfc2a03bf2254aba
Ondřej Hliba <hliba.ondra@live.com>
2019-04-23 12:27:51 +0200

    Progress on migration

--- frontend/lib/withApollo.js
+++ frontend/lib/withApollo.js
@@ -1,3 +1,4 @@
+import withApollo from 'next-with-apollo';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
@@ -5,21 +6,36 @@ import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
 import { GRAPHQL_URL } from '../config';
 
-const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.map(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: GRAPHQL_URL,
-      credentials: 'same-origin'
+export default withApollo(
+  ({ headers }) =>
+    new ApolloClient({
+      link: ApolloLink.from([
+        onError(({ graphQLErrors, networkError }) => {
+          if (graphQLErrors)
+            graphQLErrors.map(({ message, locations, path }) =>
+              console.log(
+                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+              )
+            );
+          if (networkError) console.log(`[Network error]: ${networkError}`);
+        }),
+        new HttpLink({
+          uri: GRAPHQL_URL,
+          credentials: 'include',
+          request: operation => {
+            operation.setContext({
+              fetchOptions: {
+                credentials: 'include'
+              },
+              headers
+            });
+          }
+        }),
+        new HttpLink({
+          uri: GRAPHQL_URL,
+          credentials: 'same-origin'
+        })
+      ]),
+      cache: new InMemoryCache()
     })
-  ]),
-  cache: new InMemoryCache()
-});
+);
