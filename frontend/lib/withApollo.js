046109484c10f3248265ddc295084e1033f76236
Ondřej Hliba <hliba.ondra@live.com>
2019-05-13 10:22:05 +0200

    New Apollo Client
    Need to commit before fixing backend node modules

--- frontend/lib/withApollo.js
+++ frontend/lib/withApollo.js
@@ -6,36 +6,15 @@ import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
 import { GRAPHQL_URL } from '../config';
 
+const link = createHttpLink({
+  uri: GRAPHQL_URL,
+  credentials: 'same-origin'
+});
+
 export default withApollo(
   ({ headers }) =>
     new ApolloClient({
-      link: ApolloLink.from([
-        onError(({ graphQLErrors, networkError }) => {
-          if (graphQLErrors)
-            graphQLErrors.map(({ message, locations, path }) =>
-              console.log(
-                `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-              )
-            );
-          if (networkError) console.log(`[Network error]: ${networkError}`);
-        }),
-        new HttpLink({
-          uri: GRAPHQL_URL,
-          credentials: 'include',
-          request: operation => {
-            operation.setContext({
-              fetchOptions: {
-                credentials: 'include'
-              },
-              headers
-            });
-          }
-        }),
-        new HttpLink({
-          uri: GRAPHQL_URL,
-          credentials: 'same-origin'
-        })
-      ]),
-      cache: new InMemoryCache()
+      cache: new InMemoryCache(),
+      link
     })
 );
