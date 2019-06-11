d4eb35490380f574d76f8be4648b1b455a5aef1c
Ondřej Hliba <hliba.ondra@live.com>
2019-06-11 11:29:51 +0200

    Add Mutation for setting notifications as viewed

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -527,23 +527,23 @@ const Mutations = {
           )
         );
 
-        // const mailRes = await transport.sendMail({
-        //   from: 'notifications@evently.com',
-        //   to: admin.email,
-        //   subject: 'New card has benn created',
-        //   html: cardCreated(
-        //     `
-        //   ${res.user.firstName} ${res.user.lastName} aka ${
-        //       res.user.username
-        //     } has created a new card - ${res.title}! \n\n
-        //   <a href="${process.env.FRONTEND_URL}/card?card=${res.id}&event=${
-        //       res.list.board.event.id
-        //     }&board=${res.list.board.id}&list=${
-        //       res.list.id
-        //     }">Go check it out</a>
-        //   `
-        //   )
-        // });
+        const mailRes = await transport.sendMail({
+          from: 'notifications@evently.com',
+          to: admin.email,
+          subject: 'New card has benn created',
+          html: cardCreated(
+            `
+          ${res.user.firstName} ${res.user.lastName} aka ${
+              res.user.username
+            } has created a new card - ${res.title}! \n\n
+          <a href="${process.env.FRONTEND_URL}/card?card=${res.id}&event=${
+              res.list.board.event.id
+            }&board=${res.list.board.id}&list=${
+              res.list.id
+            }">Go check it out</a>
+          `
+          )
+        });
       }
     };
 
@@ -919,6 +919,14 @@ const Mutations = {
     return {
       message: `You won't be notified!`
     };
+  },
+  async setNotificationViewed(parent, args, ctx, info) {
+    const res = await ctx.db.mutation.updateNotification(
+      { data: { viewed: true }, where: { id: args.notification } },
+      info
+    );
+
+    return res;
   }
 };
 
