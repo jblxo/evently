d1c63c169a109cb61ab5ef64e26e2be6669cf9e9
Ondřej Hliba <hliba.ondra@live.com>
2019-06-18 13:43:35 +0200

    Update UpdateCard.js

--- frontend/components/UpdateCard.js
+++ frontend/components/UpdateCard.js
@@ -6,6 +6,7 @@ import Error from './Error';
 import Form from './styles/Form';
 import Button from './styles/Button';
 import { SINGLE_CARD_QUERY } from './SingleCard';
+import User from './User';
 
 const UPDATE_CARD_MUTATION = gql`
   mutation UPDATE_CARD_MUTATION(
@@ -37,65 +38,75 @@ class UpdateCard extends React.Component {
 
   render() {
     return (
-      <Query query={SINGLE_CARD_QUERY} variables={{ id: this.props.id }}>
-        {({ data, loading }) => {
-          if (loading) return <p>Loading...</p>;
-          if (!data.card) return <p>No Card Of ID {this.props.id}</p>;
-          return (
-            <Mutation
-              mutation={UPDATE_CARD_MUTATION}
-              variables={{
-                ...this.state,
-                event: this.props.event,
-                id: this.props.id
-              }}
-              refetchQueries={[
-                { query: SINGLE_CARD_QUERY, variables: { id: this.props.id } }
-              ]}
-            >
-              {(updateCard, { loading, error }) => (
-                <Form
-                  onSubmit={async e => {
-                    e.preventDefault();
-                    const res = await updateCard();
-
-                    Router.back();
+      <User>
+        {({ data: { me } }) => (
+          <Query
+            query={SINGLE_CARD_QUERY}
+            variables={{ id: this.props.id, user: me.id }}
+          >
+            {({ data, loading }) => {
+              if (loading) return <p>Loading...</p>;
+              if (!data.card) return <p>No Card Of ID {this.props.id}</p>;
+              return (
+                <Mutation
+                  mutation={UPDATE_CARD_MUTATION}
+                  variables={{
+                    ...this.state,
+                    event: this.props.event,
+                    id: this.props.id
                   }}
+                  refetchQueries={[
+                    {
+                      query: SINGLE_CARD_QUERY,
+                      variables: { id: this.props.id, user: me.id }
+                    }
+                  ]}
                 >
-                  <fieldset disabled={loading} aria-busy={loading}>
-                    <h2>Create New Card!</h2>
-                    <Error error={error} />
-                    <label htmlFor="title">
-                      Title
-                      <input
-                        type="text"
-                        id="title"
-                        name="title"
-                        placeholder="Enter a title"
-                        defaultValue={data.card.title}
-                        onChange={this.handleChange}
-                        required
-                      />
-                    </label>
-                    <label htmlFor="description">
-                      Description
-                      <input
-                        type="text"
-                        id="description"
-                        name="description"
-                        placeholder="Enter a description"
-                        defaultValue={data.card.description}
-                        onChange={this.handleChange}
-                      />
-                    </label>
-                    <Button type="submit">Update</Button>
-                  </fieldset>
-                </Form>
-              )}
-            </Mutation>
-          );
-        }}
-      </Query>
+                  {(updateCard, { loading, error }) => (
+                    <Form
+                      onSubmit={async e => {
+                        e.preventDefault();
+                        const res = await updateCard();
+
+                        Router.back();
+                      }}
+                    >
+                      <fieldset disabled={loading} aria-busy={loading}>
+                        <h2>Update {data.card.title}!</h2>
+                        <Error error={error} />
+                        <label htmlFor="title">
+                          Title
+                          <input
+                            type="text"
+                            id="title"
+                            name="title"
+                            placeholder="Enter a title"
+                            defaultValue={data.card.title}
+                            onChange={this.handleChange}
+                            required
+                          />
+                        </label>
+                        <label htmlFor="description">
+                          Description
+                          <input
+                            type="text"
+                            id="description"
+                            name="description"
+                            placeholder="Enter a description"
+                            defaultValue={data.card.description}
+                            onChange={this.handleChange}
+                          />
+                        </label>
+                        <Button type="submit">Update</Button>
+                      </fieldset>
+                    </Form>
+                  )}
+                </Mutation>
+              );
+            }}
+          </Query>
+        )}
+      </User>
     );
   }
 }
